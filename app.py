49acc8c90a9ee00156fd2bc27f9ca03c8d783092
Abhay Chandavar <abhaychandavar11@gmail.com>
2024-12-19 22:30:48 +0530

    configured port

--- app.py
+++ app.py
@@ -34,4 +34,4 @@ async def converse(request: QueryRequest):
 
 if __name__ == "__main__":
     print('Running medAI')
-    uvicorn.run(app, host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run(app, host="0.0.0.0", port=8080, reload=True)
